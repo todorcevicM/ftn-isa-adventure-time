e3531df704217e84fe7c4c633117378ff6cc7f64
stankovictab <stankovictab@gmail.com>
2021-10-20 11:25:39 +0200

    init: Empty Maven Project

---
+++ src/main/java/isa/adventuretime/AdventureTimeApplication.java
@@ -0,0 +1,13 @@
+package isa.adventuretime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isa/adventuretime/AdventureTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.adventuretime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
